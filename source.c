a3ac692b226d1e74818a888dc3d2bf91579faff8
Minami Shota <shouth373@gmail.com>
2021-11-15 15:11:15 +0900

    ファイルの容量取得の処理の修正

--- source.c
+++ source.c
@@ -1,14 +1,156 @@
-#include <sys/stat.h>
-#include <sys/types.h>
+#define _FILE_OFFSET_BITS 64
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include "str.h"
+#include <sys/types.h>
+#include <sys/stat.h>
 
-#if defined(__linux__)
+#include "source.h"
 
-static inline stroff_t filesize(const char *filename)
+#if defined(_WIN32)
+
+static stroff_t filesize(const char *filename)
 {
-    
+    struct __stat64 s;
+    if (_stat64(filename, &s) < 0) {
+        return STR_NPOS;
+    } else {
+        if (s.st_mode & _S_IFREG) {
+            return s.st_size;
+        } else {
+            return STR_NPOS;
+        }
+    }
+}
+
+#elif defined(__unix__) || defined(__APPLE__)
+
+static stroff_t filesize(const char *filename)
+{
+    struct stat s;
+    if (stat(filename, &s) < 0) {
+        return STR_NPOS;
+    } else {
+        if (S_ISREG(s.st_mode)) {
+            return s.st_size;
+        } else {
+            return STR_NPOS;
+        }
+    }
+}
+
+#else
+
+static stroff_t filesize(const char *filename)
+{
+    const size_t block_size = 4096;
+    char buf[block_size];
+    FILE *file;
+    size_t size;
+    stroff_t ret = 0;
+
+    file = fopen(filename, "r");
+    setvbuf(file, NULL, _IOFBF, block_size);
+    do {
+        size = fread(buf, sizeof(*buf), block_size, file);
+        ret += size;
+    } while (size == block_size);
+
+    if (ferror(file) != 0) {
+        ret = STR_NPOS;
+    }
+    fclose(file);
+    return ret;
 }
 
 #endif
+
+static const char *nextline(const char *str)
+{
+    str += strcspn(str, "\r\n");
+    if (strncmp("\r\n", str, 2) == 0 || strncmp("\n\r", str, 2) == 0) {
+        return str + 2;
+    }
+    return str + 1;
+}
+
+source_t *source_new(const char *filename)
+{
+    source_t *src;
+    FILE *file;
+    size_t linecnt;
+    const char *cur;
+
+    src = (source_t *) malloc(sizeof(source_t));
+    if (src == NULL) {
+        return NULL;
+    }
+    src->filename = (char *) malloc(sizeof(char) * (strlen(filename) + 1));
+    if (src->filename == NULL) {
+        source_free(src);
+        return NULL;
+    }
+    strcpy(src->filename, filename);
+
+    src->str_size = filesize(filename);
+    if (src->str_size == STR_NPOS) {
+        source_free(src);
+        return NULL;
+    }
+
+    src->str_ptr = (char *) malloc(sizeof(char) * (src->str_size + 1));
+    if (src->str_ptr == NULL) {
+        source_free(src);
+        return NULL;
+    }
+    src->str_ptr[src->str_size] = '\0';
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        source_free(src);
+        return NULL;
+    }
+    fread(src->str_ptr, sizeof(char), src->str_size, file);
+    if (ferror(file) != 0) {
+        source_free(src);
+        return NULL;
+    }
+    fclose(file);
+
+    linecnt = 0;
+    for (cur = src->str_ptr; cur[0] != '\0'; cur = nextline(cur)) {
+        linecnt++;
+    }
+
+    src->lines_size = linecnt;
+    src->lines_ptr = (stroff_t *) malloc(sizeof(stroff_t) * src->lines_size);
+    if (src->lines_ptr == NULL) {
+        source_free(src);
+        return NULL;
+    }
+
+    linecnt = 0;
+    for (cur = src->str_ptr; cur[0] != '\0'; cur = nextline(cur)) {
+        src->lines_ptr[linecnt] = cur - src->str_ptr;
+        linecnt++;
+    }
+
+    return src;
+}
+
+int source_free(source_t *src)
+{
+    if (src != NULL) {
+        if (src->filename != NULL) {
+            free(src->filename);
+        }
+        if (src->str_ptr != NULL) {
+            free(src->str_ptr);
+        }
+        if (src->lines_ptr != NULL) {
+            free(src->lines_ptr);
+        }
+        free(src);
+    }
+}
