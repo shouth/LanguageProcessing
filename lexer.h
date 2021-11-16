729a25eb5ee35aa2fcf16ec13ded822570ce17d3
Minami Shota <shouth373@gmail.com>
2021-11-16 17:50:24 +0900

    整理

--- lexer.h
+++ lexer.h
@@ -1,13 +1,8 @@
 #ifndef LEXER_H
 #define LEXER_H
 
-#include <ctype.h>
-
 #include "strref.h"
 #include "token.h"
-
-#define LEX_SUCCESS 0
-#define LEX_FAILURE -1
 
 void lex(const strref_t *strref, token_t *ret);
 
