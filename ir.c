5340de83d17ba912a07c5568544941930301b782
Minami Shota <shouth373@gmail.com>
2022-01-31 22:07:56 +0900

    バグ修正

--- ir.c
+++ ir.c
@@ -379,12 +379,8 @@ const ir_type_t *ir_place_type(ir_place_t *place)
     assert(place);
 
     type = ir_local_type(place->local);
-    if (type->kind == IR_TYPE_ARRAY) {
-        if (place->place_access && place->place_access->kind == IR_PLACE_ACCESS_INDEX) {
-            return type->u.array_type.base_type->u.ref;
-        } else {
-            unreachable();
-        }
+    if (type->kind == IR_TYPE_ARRAY && place->place_access && place->place_access->kind == IR_PLACE_ACCESS_INDEX) {
+        return type->u.array_type.base_type->u.ref;
     } else {
         return type;
     }
