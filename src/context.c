79e914d47ba977d4423404b9ef52adacdf4efc10
Shota Minami <shouth373@gmail.com>
2023-05-03 05:40:21 +0000

    Add context

---
+++ src/context.c
@@ -0,0 +1,194 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "context.h"
+#include "source.h"
+#include "utility.h"
+
+static int symbol_comp(const void *lhs, const void *rhs)
+{
+  const symbol_t *l = lhs;
+  const symbol_t *r = rhs;
+  return l->len == r->len && !strncmp(l->ptr, r->ptr, l->len);
+}
+
+static unsigned long symbol_hash(const void *value)
+{
+  const symbol_t *x = value;
+  return fnv1a(FNV1A_SEED, x->ptr, x->len);
+}
+
+void symbol_deleter(void *value)
+{
+  symbol_t *x = value;
+  free((void *) x->ptr);
+  free(x);
+}
+
+const symbol_t *symbol(context_t *ctx, const char *ptr, long len)
+{
+  symbol_t key;
+  key.ptr = ptr;
+  key.len = len;
+
+  {
+    const hash_entry_t *entry = hash_find(ctx->symbol_interner, &key);
+    if (!entry) {
+      symbol_t *symbol = xmalloc(sizeof(symbol_t));
+      char     *str    = xmalloc(len + 1);
+      strncpy(str, ptr, len);
+      str[len]    = '\0';
+      symbol->ptr = str;
+      symbol->len = len;
+      entry       = hash_insert_unchecked(ctx->symbol_interner, symbol, NULL);
+    }
+    return entry->key;
+  }
+}
+
+static int substs_comp(const void *lhs, const void *rhs)
+{
+  const substs_t *l = lhs;
+  const substs_t *r = rhs;
+  return l->count == r->count && !memcmp(l->types, r->types, l->count);
+}
+
+static unsigned long substs_hash(const void *value)
+{
+  const substs_t *x = value;
+  return fnv1a(FNV1A_SEED, x->types, sizeof(type_t *) * x->count);
+}
+
+const substs_t *substs(context_t *ctx, const type_t **types, long count)
+{
+  substs_t key;
+  key.types = types;
+  key.count = count;
+
+  {
+    const hash_entry_t *entry = hash_find(ctx->substs_interner, &key);
+    if (!entry) {
+      substs_t *substs = xmalloc(sizeof(substs_t));
+      memcpy(substs, &key, sizeof(substs_t));
+      entry = hash_insert_unchecked(ctx->substs_interner, substs, NULL);
+    }
+    return entry->key;
+  }
+}
+
+static int type_comp(const void *lhs, const void *rhs)
+{
+  const type_t *l = lhs;
+  const type_t *r = rhs;
+
+  if (l->kind != r->kind) {
+    return 0;
+  }
+
+  switch (l->kind) {
+  case TYPE_INTEGER:
+  case TYPE_BOOLEAN:
+  case TYPE_CHAR:
+  case TYPE_STRING:
+  case TYPE_PROGRAM: {
+    return 1;
+  }
+  case TYPE_ARRAY: {
+    const type_array_t *larr = lhs;
+    const type_array_t *rarr = rhs;
+    return larr->base == rarr->base && larr->size == rarr->size;
+  }
+  case TYPE_PROCEDURE: {
+    const type_procedure_t *lproc = lhs;
+    const type_procedure_t *rproc = rhs;
+    return lproc->params == rproc->params;
+  }
+  }
+  unreachable();
+}
+
+static unsigned long type_hash(const void *value)
+{
+  const type_t *x    = value;
+  unsigned long hash = fnv1a(FNV1A_SEED, &x->kind, sizeof(type_kind_t));
+  switch (x->kind) {
+  case TYPE_ARRAY: {
+    const type_array_t *arr = value;
+    hash                    = fnv1a(hash, &arr->base, sizeof(substs_t *));
+    hash                    = fnv1a(hash, &arr->size, sizeof(long));
+    break;
+  }
+  case TYPE_PROCEDURE: {
+    const type_procedure_t *proc = value;
+    hash                         = fnv1a(hash, &proc->params, sizeof(substs_t *));
+    break;
+  }
+  case TYPE_BOOLEAN:
+  case TYPE_CHAR:
+  case TYPE_INTEGER:
+  case TYPE_STRING:
+  case TYPE_PROGRAM:
+    /* do nothing */
+    break;
+  }
+  return hash;
+}
+
+static const type_t *type_make(context_t *ctx, type_t *type, type_kind_t kind)
+{
+  type->kind = kind;
+  {
+    const hash_entry_t *entry = hash_find(ctx->type_interner, type);
+    if (!entry) {
+      type_t *ntype = xmalloc(sizeof(type_t));
+      memcpy(ntype, type, sizeof(type_t));
+      entry = hash_insert_unchecked(ctx->type_interner, ntype, NULL);
+    }
+    return entry->key;
+  }
+}
+
+const type_t *type_integer(context_t *ctx) { return ctx->type_integer; }
+const type_t *type_boolean(context_t *ctx) { return ctx->type_boolean; }
+const type_t *type_char(context_t *ctx) { return ctx->type_char; }
+const type_t *type_string(context_t *ctx) { return ctx->type_string; }
+const type_t *type_program(context_t *ctx) { return ctx->type_program; }
+
+const type_t *type_array(context_t *ctx, const substs_t *base, long size)
+{
+  type_array_t type;
+  type.base = base;
+  type.size = size;
+  return type_make(ctx, (type_t *) &type, TYPE_ARRAY);
+}
+
+const type_t *type_procedure(context_t *ctx, const substs_t *params)
+{
+  type_procedure_t type;
+  type.params = params;
+  return type_make(ctx, (type_t *) &type, TYPE_PROCEDURE);
+}
+
+context_t *ctx_init(context_t *ctx)
+{
+  ctx->symbol_interner = hash_new(&symbol_comp, &symbol_hash);
+  ctx->substs_interner = hash_new(&substs_comp, &substs_hash);
+  ctx->type_interner   = hash_new(&type_comp, &type_hash);
+
+  {
+    type_t type;
+    ctx->type_boolean = type_make(ctx, &type, TYPE_BOOLEAN);
+    ctx->type_char    = type_make(ctx, &type, TYPE_CHAR);
+    ctx->type_integer = type_make(ctx, &type, TYPE_INTEGER);
+    ctx->type_string  = type_make(ctx, &type, TYPE_STRING);
+    ctx->type_program = type_make(ctx, &type, TYPE_PROGRAM);
+  }
+  return ctx;
+}
+
+void ctx_deinit(context_t *ctx)
+{
+  hash_delete(ctx->symbol_interner, symbol_deleter, NULL);
+  hash_delete(ctx->substs_interner, free, NULL);
+  hash_delete(ctx->type_interner, free, NULL);
+}
--- src/context.h
+++ src/context.h
@@ -1,56 +1,17 @@
 #ifndef CONTEXT_H
 #define CONTEXT_H
 
-#include "ast.h"
-#include "ir.h"
 #include "source.h"
 #include "utility.h"
 
-typedef struct ref__s    ref_t;
 typedef struct symbol__s symbol_t;
-typedef struct def__s    def_t;
 typedef struct type__s   type_t;
-
-/**********     ref     **********/
-
-struct ref_s {
-  const void *ref;
-  ref_t      *next;
-};
-
-ref_t *make_ref(const void *);
 
 /**********     symbol     **********/
 
 struct symbol__s {
   const char *ptr;
   long        len;
-};
-
-/**********     definition     **********/
-
-typedef enum {
-  DEF_PROGRAM,
-  DEF_PROCEDURE,
-  DEF_VAR,
-  DEF_ARG_VAR,
-  DEF_LOCAL_VAR
-} def_kind_t;
-
-struct def__s {
-  union {
-    const ast_program_t             *program;
-    const ast_decl_part_procedure_t *procedure;
-    const ast_decl_part_variable_t  *var;
-    const ast_decl_param_t          *arg;
-    const ast_decl_part_variable_t  *local_var;
-  } def;
-
-  def_kind_t      kind;
-  const type_t   *type;
-  const symbol_t *name;
-  region_t        pos;
-  def_t          *next;
 };
 
 /**********     type     **********/
@@ -65,16 +26,22 @@ typedef enum {
   TYPE_PROCEDURE
 } type_kind_t;
 
+typedef struct substs__s         substs_t;
 typedef struct type__procedure_s type_procedure_t;
 typedef struct type__array_s     type_array_t;
 
+struct substs__s {
+  const type_t **types;
+  long           count;
+};
+
 struct type__procedure_s {
-  ref_t *params;
+  const substs_t *params;
 };
 
 struct type__array_s {
-  ref_t        *base;
-  unsigned long size;
+  const substs_t *base;
+  long            size;
 };
 
 struct type__s {
@@ -91,9 +58,8 @@ struct type__s {
 typedef struct context__s context_t;
 
 struct context__s {
-  def_t *defs;
-
   hash_t *symbol_interner;
+  hash_t *substs_interner;
   hash_t *type_interner;
 
   const type_t *type_integer;
@@ -103,14 +69,16 @@ struct context__s {
   const type_t *type_program;
 };
 
-const symbol_t *make_symbol(context_t *, const char *, long);
-
-const type_t *make_type_integer(context_t *);
-const type_t *make_type_boolean(context_t *);
-const type_t *make_type_char(context_t *);
-const type_t *make_type_string(context_t *);
-const type_t *make_type_array(context_t *, ref_t *, unsigned long);
-const type_t *make_type_program(context_t *);
-const type_t *make_type_procedure(context_t *, ref_t *);
+const symbol_t *symbol(context_t *, const char *, long);
+const substs_t *substs(context_t *, const type_t **, long);
+const type_t   *type_integer(context_t *);
+const type_t   *type_boolean(context_t *);
+const type_t   *type_char(context_t *);
+const type_t   *type_string(context_t *);
+const type_t   *type_array(context_t *, const substs_t *, long);
+const type_t   *type_program(context_t *);
+const type_t   *type_procedure(context_t *, const substs_t *);
+context_t      *ctx_init(context_t *);
+void            ctx_deinit(context_t *);
 
 #endif
