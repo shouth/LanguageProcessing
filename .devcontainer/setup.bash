b218820b4635ed041ef3fc2de0aa63f4fa106be6
Shota Minami <shouth373@gmail.com>
2023-10-19 03:00:14 +0000

    Update devcontainer settings

--- .devcontainer/setup.bash
+++ .devcontainer/setup.bash
@@ -3,4 +3,4 @@ cp .devcontainer/greeting.fish $HOME/.config/fish/conf.d/
 cp .devcontainer/llvm-toolchain.list /etc/apt/sources.list.d/
 wget -qO- https://apt.llvm.org/llvm-snapshot.gpg.key > /etc/apt/trusted.gpg.d/apt.llvm.org.asc
 
-apt update && apt install -y clangd
+apt update && apt install -y clangd-18
