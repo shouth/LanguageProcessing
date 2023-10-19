1e3efe6ba704e08b11039db57aa7916c8bb765f6
Shota Minami <shouth373@gmail.com>
2023-10-19 00:54:21 +0000

    Update parser definition

--- src/parser.c
+++ src/parser.c
@@ -1,5 +1,57 @@
-#include "lexical_tree.h"
+#include <string.h>
 
-int parse(const char *source, long length, LexicalTree *tree)
+#include "lexer.h"
+#include "parser.h"
+
+void parser_init(Parser *parser, const char *source, unsigned long size)
 {
+  parser->_source = source;
+  parser->_size   = size;
+  parser->_offset = 0;
+  lexer_init(&parser->_lexer, source, size);
+  parser_bump(parser);
 }
+
+void parser_token(Parser *parser, LexerToken *token)
+{
+  *token = parser->_token;
+}
+
+void parser_bump(Parser *parser)
+{
+  lexer_next_token(&parser->_lexer, &parser->_token);
+  parser->_offset += parser->_token.size;
+}
+
+int parser_check(Parser *parser, LexerTokenKind kind)
+{
+  return parser->_token.kind == kind;
+}
+
+int parser_check_keyword(Parser *parser, const char *keyword)
+{
+  return parser_check(parser, LEXER_TOKEN_KIND_IDENTIFIER)
+    && !strncmp(parser->_source + parser->_offset - parser->_token.size, keyword, parser->_token.size);
+}
+
+int parser_eat(Parser *parser, LexerTokenKind kind)
+{
+  int result = parser_check(parser, kind);
+  if (result) {
+    parser_bump(parser);
+  }
+  return result;
+}
+
+int parser_eat_keyword(Parser *parser, const char *keyword)
+{
+  int result = parser_check_keyword(parser, keyword);
+  if (result) {
+    parser_bump(parser);
+  }
+  return result;
+}
+
+int parser_expect(Parser *parser, LexerTokenKind kind);
+
+int parser_expect_keyword(Parser *parser, const char *keyword);
--- src/parser.h
+++ src/parser.h
@@ -2,12 +2,28 @@
 #define PARSER_H
 
 #include "lexer.h"
-#include "lexical_tree.h"
+#include "list.h"
+
+typedef struct ParserCursor ParserCursor;
+typedef struct Parser       Parser;
 
 struct Parser {
-  Lexer lexer;
+  const char   *_source;
+  unsigned long _size;
+  unsigned long _offset;
+  Lexer         _lexer;
+  LexerToken    _token;
+  List          _errors;
 };
 
-int parse(const char *source, long length, LexicalTree *tree);
+void parser_init(Parser *parser, const char *source, unsigned long size);
+void parser_token(Parser *parser, LexerToken *token);
+void parser_bump(Parser *parser);
+int  parser_check(Parser *parser, LexerTokenKind kind);
+int  parser_check_keyword(Parser *parser, const char *keyword);
+int  parser_eat(Parser *parser, LexerTokenKind kind);
+int  parser_eat_keyword(Parser *parser, const char *keyword);
+int  parser_expect(Parser *parser, LexerTokenKind kind);
+int  parser_expect_keyword(Parser *parser, const char *keyword);
 
 #endif
