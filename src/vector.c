5b8e416a03bbcde48be8050c9069c5e13ab86eae
Shota Minami <shouth373@gmail.com>
2023-10-20 09:39:22 +0000

    Add vector

---
+++ src/vector.c
@@ -0,0 +1,67 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "vector.h"
+
+void vector_init(Vector *vector)
+{
+  vector_init_with_capacity(vector, 1 << 4);
+}
+
+void vector_init_with_capacity(Vector *vector, unsigned long capacity)
+{
+  vector->_size = 0;
+  vector->_data = NULL;
+  vector_reserve(vector, capacity);
+}
+
+void vector_deinit(Vector *vector)
+{
+  free(vector->_data);
+}
+
+unsigned long vector_size(const Vector *vector)
+{
+  return vector->_size;
+}
+
+unsigned long vector_capacity(const Vector *vector)
+{
+  return vector->_capacity;
+}
+
+void **vector_data(const Vector *vector)
+{
+  return vector->_data;
+}
+
+void vector_reserve(Vector *vector, unsigned long capacity)
+{
+  void **new = realloc(vector->_data, sizeof(void *) * capacity);
+  if (!new) {
+    fprintf(stderr, "Internal Error: Failed to allocate memory\n");
+    exit(EXIT_FAILURE);
+  }
+  vector->_data     = new;
+  vector->_capacity = capacity;
+}
+
+void vector_push(Vector *vector, void *value)
+{
+  if (vector->_size == vector->_capacity) {
+    vector_reserve(vector, vector->_capacity * 2);
+  }
+  vector->_data[vector->_size] = value;
+  ++vector->_size;
+}
+
+void vector_pop(Vector *vector)
+{
+  --vector->_size;
+}
+
+void vector_clear(Vector *vector)
+{
+  vector->_size = 0;
+}
---
+++ src/vector.h
@@ -0,0 +1,24 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+typedef struct Vector Vector;
+
+struct Vector {
+  void        **_data;
+  unsigned long _size;
+  unsigned long _capacity;
+};
+
+void vector_init(Vector *vector);
+void vector_init_with_capacity(Vector *vector, unsigned long capacity);
+void vector_deinit(Vector *vector);
+
+unsigned long vector_size(const Vector *vector);
+unsigned long vector_capacity(const Vector *vector);
+void        **vector_data(const Vector *vector);
+void          vector_reserve(Vector *vector, unsigned long capacity);
+void          vector_push(Vector *vector, void *value);
+void          vector_pop(Vector *vector);
+void          vector_clear(Vector *vector);
+
+#endif
