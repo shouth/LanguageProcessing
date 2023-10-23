a4b2765f054e53e0ea81fa569eea758b0796c00b
Shota Minami <shouth373@gmail.com>
2023-10-23 00:04:49 +0000

    WIP

--- src/main.c
+++ src/main.c
@@ -1,84 +1,9 @@
-#include <assert.h>
-#include <limits.h>
-#include <stdio.h>
 #include <stdlib.h>
 
-#include <sys/stat.h>
-
-#include "lexer.h"
-#include "map.h"
-#include "token.h"
+#include "tasks.h"
 
 int main(int argc, const char **argv)
 {
-  Map map;
-  map_init(&map, NULL, NULL);
-
-  {
-    long i, size = 100000;
-    for (i = 0; i < size; ++i) {
-      map_insert(&map, (void *) i, (void *) i);
-    }
-
-    printf("insertion done\n");
-
-    for (i = 0; i < size; ++i) {
-      MapIndex index;
-      if (!map_find(&map, (void *) i, &index)) {
-        printf("%ld not found\n", i);
-      }
-    }
-
-    printf("lookup done\n");
-  }
-
-  /*
-  if (argc < 2) {
-    fprintf(stderr, "Usage: %s <filename>", argv[0]);
-    return EXIT_FAILURE;
-  }
-
-  {
-    char *source;
-    long  source_size;
-    {
-      struct stat s;
-      if (stat(argv[1], &s) && !S_ISREG(s.st_mode)) {
-        fprintf(stderr, "`%s` is not a valid file\n", argv[1]);
-        return EXIT_FAILURE;
-      }
-      source = malloc(s.st_size);
-      source_size = s.st_size;
-    }
-    {
-      FILE *file = fopen(argv[1], "r");
-      if (!file) {
-        fprintf(stderr, "Cannot open file `%s`\n", argv[1]);
-        return EXIT_FAILURE;
-      }
-      fread(source, sizeof source[0], source_size, file);
-      fclose(file);
-    }
-    {
-      Lexer lexer;
-      long  offset = 0;
-      Token token;
-      lexer_init(&lexer, source, source_size);
-
-      do {
-        lexer_next_token(&lexer, &token);
-
-        printf("kind: %4d", token.kind);
-        printf("\tlength: %4ld", token.length);
-        if (token.kind != TOKEN_KIND_SPACE && token.kind != TOKEN_KIND_NEWLINE && token.kind != TOKEN_KIND_EOF) {
-          printf("\ttoken: %.*s", (int) token.length, source + offset);
-        }
-        printf("\n");
-
-        offset += token.length;
-      } while (token.kind != TOKEN_KIND_EOF);
-    }
-  }
-*/
+  task1(argc, argv);
   return EXIT_SUCCESS;
 }
--- src/parser.c
+++ src/parser.c
@@ -1,8 +1,144 @@
 #include <string.h>
 
 #include "lexer.h"
+#include "lexical_tree.h"
 #include "parser.h"
 #include "symbol.h"
+
+static void bump(Parser *parser)
+{
+  lexer_next_token(&parser->_lexer, &parser->_token);
+  parser->_offset += parser->_token.size;
+}
+
+static int check(Parser *parser, LexerTokenKind kind)
+{
+  return parser->_token.kind == kind;
+}
+
+static int check_keyword(Parser *parser, Symbol keyword)
+{
+  return check(parser, LEXER_TOKEN_KIND_IDENTIFIER)
+    && !strncmp(parser->_source + parser->_offset - parser->_token.size, symbol_string(keyword), parser->_token.size);
+}
+
+static int check_identifier(Parser *parser)
+{
+  Symbol token = symbol_from(parser->_source + parser->_offset - parser->_token.size, parser->_token.size);
+  return check(parser, LEXER_TOKEN_KIND_IDENTIFIER) && !symbol_is_keyword(token);
+}
+
+static int eat(Parser *parser, LexerTokenKind kind)
+{
+  int result = check(parser, kind);
+  if (result) {
+    bump(parser);
+  }
+  return result;
+}
+
+static int eat_keyword(Parser *parser, Symbol keyword)
+{
+  int result = check_keyword(parser, keyword);
+  if (result) {
+    bump(parser);
+  }
+  return result;
+}
+
+static int eat_identifier(Parser *parser)
+{
+  int result = check_identifier(parser);
+  if (result) {
+    bump(parser);
+  }
+  return result;
+}
+
+static int expect(Parser *parser, LexerTokenKind kind)
+{
+  int result = eat(parser, kind);
+  if (!result) {
+    /* TODO: create an error object and push to parser */
+  }
+  return result;
+}
+
+static int expect_keyword(Parser *parser, Symbol keyword)
+{
+  int result = eat_keyword(parser, keyword);
+  if (!result) {
+    /* TODO: create an error object and push to parser */
+  }
+  return result;
+}
+
+static int expect_identifier(Parser *parser)
+{
+  int result = eat_identifier(parser);
+  if (!result) {
+    /* TODO: create an error object and push to parser */
+  }
+  return result;
+}
+
+static void node_start(Parser *parser, SyntaxKind kind)
+{
+}
+
+static void node_end(Parser *parser)
+{
+}
+
+static void parse_compound_statement(Parser *parser)
+{
+  node_start(parser, SYNTAX_KIND_COMPOUND_STATEMENT);
+
+  node_end(parser);
+}
+
+static void parse_variable_declaration(Parser *parser)
+{
+  node_start(parser, SYNTAX_KIND_VARIABLE_DECLARATOIN);
+
+  node_end(parser);
+}
+
+static void parse_subprogram_declaration(Parser *parser)
+{
+  node_start(parser, SYNTAX_KIND_SUBPROGRAM_DECLARATION);
+  expect_keyword(parser, SYMBOL_KEYWORD_PROCEDURE);
+  expect_identifier(parser);
+
+  node_end(parser);
+}
+
+static void parse_block(Parser *parser)
+{
+  node_start(parser, SYNTAX_KIND_BLOCK);
+  while (1) {
+    if (check_keyword(parser, SYMBOL_KEYWORD_VAR)) {
+      parse_variable_declaration(parser);
+    } else if (check_keyword(parser, SYMBOL_KEYWORD_PROCEDURE)) {
+      parse_subprogram_declaration(parser);
+    } else {
+      break;
+    }
+  }
+  parse_compound_statement(parser);
+  node_end(parser);
+}
+
+static void parse_program(Parser *parser)
+{
+  node_start(parser, SYNTAX_KIND_PROGRAM);
+  expect_keyword(parser, SYMBOL_KEYWORD_PROGRAM);
+  expect_identifier(parser);
+  expect(parser, LEXER_TOKEN_KIND_SEMICOLON);
+  parse_block(parser);
+  expect(parser, LEXER_TOKEN_KIND_DOT);
+  node_end(parser);
+}
 
 void parser_init(Parser *parser, const char *source, unsigned long size)
 {
@@ -10,44 +146,5 @@ void parser_init(Parser *parser, const char *source, unsigned long size)
   parser->_size   = size;
   parser->_offset = 0;
   lexer_init(&parser->_lexer, source, size);
-  parser_bump(parser);
+  bump(parser);
 }
-
-void parser_bump(Parser *parser)
-{
-  lexer_next_token(&parser->_lexer, &parser->_token);
-  parser->_offset += parser->_token.size;
-}
-
-int parser_check(Parser *parser, LexerTokenKind kind)
-{
-  return parser->_token.kind == kind;
-}
-
-int parser_check_keyword(Parser *parser, Symbol keyword)
-{
-  return parser_check(parser, LEXER_TOKEN_KIND_IDENTIFIER)
-    && !strncmp(parser->_source + parser->_offset - parser->_token.size, symbol_string(keyword), parser->_token.size);
-}
-
-int parser_eat(Parser *parser, LexerTokenKind kind)
-{
-  int result = parser_check(parser, kind);
-  if (result) {
-    parser_bump(parser);
-  }
-  return result;
-}
-
-int parser_eat_keyword(Parser *parser, Symbol keyword)
-{
-  int result = parser_check_keyword(parser, keyword);
-  if (result) {
-    parser_bump(parser);
-  }
-  return result;
-}
-
-int parser_expect(Parser *parser, LexerTokenKind kind);
-
-int parser_expect_keyword(Parser *parser, Symbol keyword);
