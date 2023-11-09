c63e95df25869292c686a2895fca4407e73a1984
Shota Minami <shouth373@gmail.com>
2023-11-09 23:48:36 +0000

    Update main

--- src/main.c
+++ src/main.c
@@ -1,9 +1,17 @@
 #include <stdlib.h>
 
+#include "module.h"
 #include "tasks.h"
+#include "token.h"
 
 int main(int argc, const char **argv)
 {
-  task1(argc, argv);
+  Module    module;
+  TokenTree tree;
+  module_init(&module, argv[1]);
+  module_token_tree(&module, &tree);
+  token_node_print((TokenNode *) &tree);
+  token_tree_deinit(&tree);
+  module_deinit(&module);
   return EXIT_SUCCESS;
 }
