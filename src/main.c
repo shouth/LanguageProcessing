608d1162978cf4ca635eae875eb871ec35bf3bf6
Shota Minami <shouth373@gmail.com>
2023-11-14 02:40:30 +0000

    Update main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
+#include <stddef.h>
 #include <stdlib.h>
 
 #include "module.h"
+#include "pretty_printer.h"
 #include "tasks.h"
 #include "token.h"
 
@@ -10,7 +12,7 @@ int main(int argc, const char **argv)
   TokenTree tree;
   module_init(&module, argv[1]);
   module_token_tree(&module, &tree);
-  token_node_print((TokenNode *) &tree);
+  mppl_pretty_print((TokenNode *) &tree, NULL);
   token_tree_deinit(&tree);
   module_deinit(&module);
   return EXIT_SUCCESS;
