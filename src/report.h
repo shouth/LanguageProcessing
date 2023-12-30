917864f1a4caf7e72751a39cfa9ca61d3a4f5842
Shota Minami <shouth373@gmail.com>
2023-12-30 12:44:15 +0000

    Refactor report

--- src/report.h
+++ src/report.h
@@ -15,29 +15,13 @@ typedef enum {
 typedef struct ReportAnnotation ReportAnnotation;
 typedef struct Report           Report;
 
-struct ReportAnnotation {
-  unsigned long  _start_offset;
-  unsigned long  _end_offset;
-  SourceLocation _start;
-  SourceLocation _end;
-  char          *_message;
-};
-
-struct Report {
-  ReportKind    _kind;
-  unsigned long _offset;
-  char         *_message;
-  Array        *_annotations;
-  Array        *_notes;
-};
-
-void report_init(Report *report, ReportKind kind, unsigned long offset, const char *format, ...);
-void report_init_with_args(Report *report, ReportKind kind, unsigned long offset, const char *format, va_list args);
-void report_deinit(Report *report);
-void report_annotation(Report *report, unsigned long start, unsigned long end, const char *format, ...);
-void report_annotation_with_args(Report *report, unsigned long start, unsigned long end, const char *format, va_list args);
-void report_note(Report *report, const char *format, ...);
-void report_note_with_args(Report *report, const char *format, va_list args);
-void report_emit(Report *report, const Source *source);
+Report *report_new(ReportKind kind, unsigned long offset, const char *format, ...);
+Report *report_new_with_args(ReportKind kind, unsigned long offset, const char *format, va_list args);
+void    report_free(Report *report);
+void    report_annotation(Report *report, unsigned long start, unsigned long end, const char *format, ...);
+void    report_annotation_with_args(Report *report, unsigned long start, unsigned long end, const char *format, va_list args);
+void    report_note(Report *report, const char *format, ...);
+void    report_note_with_args(Report *report, const char *format, va_list args);
+void    report_emit(Report *report, const Source *source);
 
 #endif
