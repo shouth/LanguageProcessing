295908c899adc89dda61c4a86c651bd598d4cd0c
Shota Minami <shouth373@gmail.com>
2024-01-08 12:40:50 +0000

    Add extra functions to mppl syntax

---
+++ src/mppl_syntax_ext.c
@@ -0,0 +1,76 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "mppl_syntax.h"
+#include "mppl_syntax_ext.h"
+#include "syntax_tree.h"
+#include "token_tree.h"
+#include "type.h"
+#include "utility.h"
+
+Type *mppl_std_type__to_type(const AnyMpplStdType *syntax)
+{
+  switch (mppl_std_type__kind(syntax)) {
+  case MPPL_STD_TYPE_BOOLEAN:
+    return type_new(TYPE_BOOLEAN);
+
+  case MPPL_STD_TYPE_CHAR:
+    return type_new(TYPE_CHAR);
+
+  case MPPL_STD_TYPE_INTEGER:
+    return type_new(TYPE_INTEGER);
+
+  default:
+    unreachable();
+  }
+}
+
+Type *mppl_type__to_type(const AnyMpplType *syntax)
+{
+  switch (mppl_type__kind(syntax)) {
+  case MPPL_TYPE_STD:
+    return mppl_std_type__to_type((const AnyMpplStdType *) syntax);
+
+  case MPPL_TYPE_ARRAY: {
+    const MpplArrayType *array_syntax = (const MpplArrayType *) syntax;
+    AnyMpplStdType      *elem_syntax  = mppl_array_type__type(array_syntax);
+    MpplLitNumber       *size_syntax  = mppl_array_type__size(array_syntax);
+    Type                *elem_type    = mppl_std_type__to_type(elem_syntax);
+    long                 size         = mppl_lit_number__to_long(size_syntax);
+
+    mppl_free(size_syntax);
+    mppl_free(elem_syntax);
+    return type_new_array(elem_type, size);
+  }
+
+  default:
+    unreachable();
+  }
+}
+
+long mppl_lit_number__to_long(const MpplLitNumber *syntax)
+{
+  const Token *token = (const Token *) syntax_tree_raw((const SyntaxTree *) syntax);
+  return atol(token->text);
+}
+
+char *mppl_lit_string__to_string(const MpplLitString *syntax)
+{
+  const Token  *token  = (const Token *) syntax_tree_raw((const SyntaxTree *) syntax);
+  char         *result = xmalloc(token->text_length + 1);
+  unsigned long i, j;
+  for (i = 0, j = 0; i < token->text_length; ++i, ++j) {
+    if (token->text[i] == '\'') {
+      ++i;
+    }
+    result[j] = token->text[i];
+  }
+  result[j] = '\0';
+  return result;
+}
+
+int mppl_lit_boolean__to_int(const MpplLitBoolean *syntax)
+{
+  const Token *token = (const Token *) syntax_tree_raw((const SyntaxTree *) syntax);
+  return !strcmp(token->text, "true");
+}
---
+++ src/mppl_syntax_ext.h
@@ -0,0 +1,17 @@
+#ifndef MPPL_SYNTAX_EXT_H
+#define MPPL_SYNTAX_EXT_H
+
+#include "mppl_syntax.h"
+#include "type.h"
+
+Type *mppl_std_type__to_type(const AnyMpplStdType *syntax);
+
+Type *mppl_type__to_type(const AnyMpplType *syntax);
+
+long mppl_lit_number__to_long(const MpplLitNumber *syntax);
+
+char *mppl_lit_string__to_string(const MpplLitString *syntax);
+
+int mppl_lit_boolean__to_int(const MpplLitBoolean *syntax);
+
+#endif
