4b4c3d3ea43e51a6607386630e4534a0ee25b76e
Shota Minami <shouth373@gmail.com>
2024-09-09 02:16:41 +0000

    Add new hash map (but still buggy)

--- src/utility.c
+++ src/utility.c
@@ -14,11 +14,14 @@
    limitations under the License.
 */
 
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "utility.h"
+
+/* Memory */
 
 void *xmalloc(unsigned long size)
 {
@@ -113,17 +116,6 @@ void *splice_alloc(
   return result;
 }
 
-Hash hash_fnv1a(Hash *hash, const void *ptr, unsigned long len)
-{
-  unsigned long i;
-
-  Hash result = hash ? *hash : 0x811C9DC5ul;
-  for (i = 0; i < len; ++i) {
-    result = 0xFFFFFFFFul & ((result ^ *(unsigned char *) ptr) * 0x01000193ul);
-  }
-  return *hash = result;
-}
-
 unsigned long popcount(const void *data, unsigned long count)
 {
 #define B2(n) n, n + 1, n + 1, n + 2
@@ -145,6 +137,154 @@ unsigned long popcount(const void *data, unsigned long count)
   }
   return result;
 }
+
+/* Hash */
+
+unsigned long hash_fnv1a(unsigned long *hash, const void *ptr, unsigned long len)
+{
+  unsigned long i;
+
+  unsigned long result = hash ? *hash : 0x811C9DC5ul;
+  for (i = 0; i < len; ++i) {
+    result = 0xFFFFFFFFul & ((result ^ *(unsigned char *) ptr) * 0x01000193ul);
+  }
+  if (hash) {
+    *hash = result;
+  }
+  return result;
+}
+
+/* Hopscotch */
+
+#define BUCKET_SIZE (sizeof(unsigned long) * CHAR_BIT)
+
+void hopscotch_alloc(Hopscotch *hopscotch, unsigned long span, HopscotchHash *hash, HopscotchEq *eq)
+{
+  seq_alloc(&hopscotch->hops, span);
+  memset(hopscotch->hops.ptr, 0, span * sizeof(unsigned long));
+  hopscotch->hash = hash;
+  hopscotch->eq   = eq;
+}
+
+void hopscotch_free(Hopscotch *hopscotch)
+{
+  seq_free(&hopscotch->hops);
+}
+
+void hopscotch_unchecked(Hopscotch *hopscotch, const void *key, HopscotchEntry *entry)
+{
+  if (key && hopscotch->hops.span > 0) {
+    entry->bucket = hopscotch->hash(key) % hopscotch->hops.span;
+  } else {
+    entry->bucket = hopscotch->hops.span;
+  }
+  entry->slot = BUCKET_SIZE;
+}
+
+int hopscotch_entry(Hopscotch *hopscotch, void *data, unsigned long size, const void *key, HopscotchEntry *entry)
+{
+  hopscotch_unchecked(hopscotch, key, entry);
+  if (entry->bucket < hopscotch->hops.span) {
+    for (entry->slot = 0; entry->slot < BUCKET_SIZE; ++entry->slot) {
+      if (hopscotch->hops.ptr[entry->bucket] & (1ul << entry->slot)) {
+        if (hopscotch->eq((char *) data + (entry->bucket + entry->slot) * size, key)) {
+          return 1;
+        }
+      }
+    }
+  }
+  return 0;
+}
+
+int hopscotch_next(Hopscotch *hopscotch, HopscotchEntry *entry)
+{
+  if (entry->bucket < hopscotch->hops.span) {
+    ++entry->slot;
+  } else {
+    entry->bucket = 0;
+    entry->slot   = 0;
+  }
+
+  for (; entry->bucket < hopscotch->hops.span; ++entry->bucket) {
+    for (; entry->slot < BUCKET_SIZE; ++entry->slot) {
+      if (hopscotch->hops.ptr[entry->bucket] & (1ul << entry->slot)) {
+        return 1;
+      }
+    }
+    entry->slot = 0;
+  }
+  return 0;
+}
+
+int hopscotch_occupy(Hopscotch *hopscotch, void *data, unsigned long size, HopscotchEntry *entry)
+{
+  if (hopscotch->hops.span == 0 || entry->bucket >= hopscotch->hops.span) {
+    return 0;
+  } else if (entry->slot < BUCKET_SIZE) {
+    return -1;
+  } else if (hopscotch->hops.ptr[entry->bucket] + 1 == 0) {
+    return 0;
+  } else {
+    unsigned long bucket, slot;
+    unsigned long occupied = 0;
+    unsigned long empty    = -1ul;
+
+    bucket = entry->bucket > BUCKET_SIZE - 1ul ? entry->bucket - (BUCKET_SIZE - 1ul) : 0ul;
+    for (; bucket < entry->bucket + BUCKET_SIZE * 8ul; ++bucket) {
+      occupied >>= 1ul;
+      if (bucket < hopscotch->hops.span) {
+        occupied |= hopscotch->hops.ptr[bucket];
+      }
+      if (bucket >= entry->bucket && ~occupied & 1ul) {
+        empty = bucket;
+        break;
+      }
+    }
+
+    if (empty == -1ul) {
+      return 0;
+    }
+
+    while (empty - entry->bucket >= BUCKET_SIZE) {
+      for (bucket = empty - (BUCKET_SIZE - 1ul); bucket < empty; ++bucket) {
+        printf("%lu %lu %lu %lx\n", bucket, empty, hopscotch->hops.span, hopscotch->hops.ptr[bucket]);
+        if (hopscotch->hops.ptr[bucket] & ((1ul << (empty - bucket)) - 1ul)) {
+          break;
+        }
+      }
+
+      if (bucket == empty) {
+        return 0;
+      }
+
+      for (slot = 0; bucket + slot < empty; ++slot) {
+        if (hopscotch->hops.ptr[bucket] & (1ul << slot)) {
+          break;
+        }
+      }
+
+      hopscotch->hops.ptr[bucket] &= ~(1ul << slot);
+      hopscotch->hops.ptr[bucket] |= 1ul << (empty - bucket);
+      memcpy((char *) data + (bucket + slot) * size, (char *) data + empty * size, size);
+      empty = bucket + slot;
+    }
+
+    entry->slot = empty - entry->bucket;
+    hopscotch->hops.ptr[entry->bucket] |= 1ul << entry->slot;
+    return 1;
+  }
+}
+
+void hopscotch_release(Hopscotch *hopscotch, HopscotchEntry *entry)
+{
+  if (entry->bucket < hopscotch->hops.span && entry->slot < BUCKET_SIZE) {
+    hopscotch->hops.ptr[entry->bucket] &= ~(1ul << entry->slot);
+  }
+}
+
+#undef BUCKET_SIZE
+
+/* Charactor */
 
 int is_alphabet(int c)
 {
--- src/utility.h
+++ src/utility.h
@@ -187,6 +187,143 @@ Hash hash_fnv1a(Hash *hash, const void *ptr, unsigned long len);
     (vec)->count = 0;  \
   } while (0)
 
+/* Hopscotch */
+
+typedef unsigned long         HopscotchHash(const void *ptr);
+typedef int                   HopscotchEq(const void *lhs, const void *rhs);
+typedef struct HopscotchEntry HopscotchEntry;
+typedef struct Hopscotch      Hopscotch;
+
+struct HopscotchEntry {
+  unsigned long bucket;
+  unsigned long slot;
+};
+
+struct Hopscotch {
+  Seq(unsigned long) hops;
+  HopscotchHash *hash;
+  HopscotchEq   *eq;
+};
+
+void hopscotch_alloc(Hopscotch *hopscotch, unsigned long span, HopscotchHash *hash, HopscotchEq *eq);
+void hopscotch_free(Hopscotch *hopscotch);
+void hopscotch_unchecked(Hopscotch *hopscotch, const void *key, HopscotchEntry *entry);
+int  hopscotch_entry(Hopscotch *hopscotch, void *data, unsigned long size, const void *key, HopscotchEntry *entry);
+int  hopscotch_next(Hopscotch *hopscotch, HopscotchEntry *entry);
+int  hopscotch_occupy(Hopscotch *hopscotch, void *data, unsigned long size, HopscotchEntry *entry);
+void hopscotch_release(Hopscotch *hopscotch, HopscotchEntry *entry);
+
+/* HashMap */
+
+typedef HopscotchEntry HashMapEntry;
+
+#define HashMap(key_type, value_type) \
+  struct {                            \
+    Hopscotch metadata;               \
+    struct {                          \
+      union {                         \
+        key_type mutable;             \
+        key_type const readonly;      \
+      } key;                          \
+      value_type value;               \
+    }            *ptr;                \
+    unsigned long span;               \
+    unsigned long count;              \
+  }
+
+#define hashmap_alloc(map, hash, eq)                \
+  do {                                              \
+    hopscotch_alloc(&(map)->metadata, 0, hash, eq); \
+    seq_alloc(map, 0);                              \
+    (map)->count = 0;                               \
+  } while (0)
+
+#define hashmap_free(map)             \
+  do {                                \
+    hopscotch_free(&(map)->metadata); \
+    seq_free(map);                    \
+  } while (0)
+
+#define hashmap_reserve(map, new_capacity)                                                      \
+  do {                                                                                          \
+    unsigned long capacity = new_capacity;                                                      \
+    if (capacity + sizeof(unsigned long) * CHAR_BIT - 1 > (map)->span) {                        \
+      void         *old_ptr  = (map)->ptr;                                                      \
+      unsigned long old_span = (map)->span;                                                     \
+      Hopscotch     metadata = (map)->metadata;                                                 \
+      int           finished = 0;                                                               \
+      unsigned long i, hop;                                                                     \
+                                                                                                \
+      --capacity;                                                                               \
+      for (i = 1; i < sizeof(i) * CHAR_BIT; i <<= 1) {                                          \
+        capacity |= capacity >> i;                                                              \
+      }                                                                                         \
+      ++capacity;                                                                               \
+                                                                                                \
+      while (!finished) {                                                                       \
+        hopscotch_alloc(&(map)->metadata, capacity, metadata.hash, metadata.eq);                \
+        seq_alloc(map, capacity + sizeof(unsigned long) * CHAR_BIT - 1);                        \
+                                                                                                \
+        finished = 1;                                                                           \
+        hop      = 0;                                                                           \
+        for (i = 0; i < old_span; ++i) {                                                        \
+          hop >>= 1;                                                                            \
+          if (i < metadata.hops.span) {                                                         \
+            hop |= metadata.hops.ptr[i];                                                        \
+          }                                                                                     \
+          if (hop & 1ul) {                                                                      \
+            const void    *kv = (char *) old_ptr + i * sizeof(*(map)->ptr);                     \
+            HopscotchEntry entry;                                                               \
+            hopscotch_unchecked(&(map)->metadata, kv, &entry);                                  \
+            if (!hopscotch_occupy(&(map)->metadata, (map)->ptr, sizeof(*(map)->ptr), &entry)) { \
+              finished = 0;                                                                     \
+              capacity *= 2;                                                                    \
+              hopscotch_free(&(map)->metadata);                                                 \
+              seq_free(map);                                                                    \
+              break;                                                                            \
+            }                                                                                   \
+            memcpy((map)->ptr + (entry.bucket + entry.slot), kv, sizeof(*(map)->ptr));          \
+          }                                                                                     \
+        }                                                                                       \
+      }                                                                                         \
+                                                                                                \
+      free(old_ptr);                                                                            \
+      hopscotch_free(&metadata);                                                                \
+    }                                                                                           \
+  } while (0);
+
+#define hashmap_entry(map, key, entry)                                                 \
+  (key                                                                                 \
+      ? hopscotch_entry(&(map)->metadata, (map)->ptr, sizeof(*(map)->ptr), key, entry) \
+      : (hopscotch_unchecked(&(map)->metadata, NULL, entry), 0))
+
+#define hashmap_key(map, entry)                                                      \
+  ((entry)->bucket < (map)->span && (entry)->slot < sizeof(unsigned long) * CHAR_BIT \
+      ? &(map)->ptr[(entry)->bucket + (entry)->slot].key.readonly                    \
+      : NULL)
+
+#define hashmap_value(map, entry)                                                    \
+  ((entry)->bucket < (map)->span && (entry)->slot < sizeof(unsigned long) * CHAR_BIT \
+      ? &(map)->ptr[(entry)->bucket + (entry)->slot].value                           \
+      : NULL)
+
+#define hashmap_next(map, entry) \
+  hopscotch_next(&(map)->metadata, entry)
+
+#define hashmap_update(map, entry, new_key, new_value)                                                 \
+  do {                                                                                                 \
+    int status;                                                                                        \
+    while (!(status = hopscotch_occupy(&(map)->metadata, (map)->ptr, sizeof(*(map)->ptr), (entry)))) { \
+      hashmap_reserve(map, (map)->metadata.hops.span ? (map)->metadata.hops.span * 2 : 1);             \
+      hopscotch_unchecked(&(map)->metadata, (new_key), (entry));                                       \
+    }                                                                                                  \
+    if (status == 1) {                                                                                 \
+      ++(map)->count;                                                                                  \
+    }                                                                                                  \
+    (map)->ptr[(entry)->bucket + (entry)->slot].key.mutable = *(new_key);                              \
+    (map)->ptr[(entry)->bucket + (entry)->slot].value       = *(new_value);                            \
+  } while (0)
+
 /* Character */
 
 int is_alphabet(int c);
