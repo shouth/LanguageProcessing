e60f38bc57b1b84ca2f59bd275e0374e50a7563d
Shota Minami <shouth373@gmail.com>
2026-04-17 08:19:50 +0900

    add tests for data structures

---
+++ test/ht.c
@@ -0,0 +1,102 @@
+/*
+ * ht.c -- hash table test
+ *
+ * SPDX-FileCopyrightText: 2026 Shota Minami
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define _POSIX_C_SOURCE 200809L
+
+#include <assert.h>
+#include <limits.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "ds.h"
+
+#define SIZE 10000000
+
+unsigned long hash(void *x)
+{
+  unsigned long v = *(unsigned long *) x;
+  return (((v << 5) | v >> (sizeof(unsigned long) * CHAR_BIT - 5)) ^ v) * 0x517cc1b727220a95UL;
+}
+
+int eq(void *l, void *r)
+{
+  return *(unsigned long *) l == *(unsigned long *) r;
+}
+
+unsigned long *make_shuffled(size_t n)
+{
+  unsigned long *a = malloc(sizeof(unsigned long) * n);
+  size_t i;
+
+  for (i = 0; i < n; ++i) {
+    a[i] = i;
+  }
+  for (i = 0; i + 1 < n; ++i) {
+    size_t j = rand() % (n - (i + 1)) + i;
+    unsigned long t = a[i];
+    a[i] = a[j];
+    a[j] = t;
+  }
+  return a;
+}
+
+int main(void)
+{
+  unsigned long i;
+  struct ht_entry e;
+
+  unsigned long *keys, *values;
+  hm(unsigned long, unsigned long) m;
+
+  struct timespec start, end;
+  unsigned long ns;
+
+  keys = make_shuffled(SIZE);
+  values = make_shuffled(SIZE);
+
+  ht_init(&m, hash, eq);
+
+  clock_gettime(CLOCK_MONOTONIC, &start);
+
+  for (i = 0; i < SIZE; ++i) {
+    ht_entry(&m, &keys[i], &e);
+    ht_occupy(&m, &e, &keys[i]);
+    ht_at(&m, &e)->value = values[i];
+  }
+
+  clock_gettime(CLOCK_MONOTONIC, &end);
+  ns = (end.tv_sec - start.tv_sec) * 1000000000UL + (end.tv_nsec - start.tv_nsec);
+  printf("insert:\n  total: %lu ns\n  average: %lu ns\n", ns, ns / SIZE);
+
+  assert(m.count == SIZE);
+
+  clock_gettime(CLOCK_MONOTONIC, &start);
+
+  for (i = 0; i < SIZE; ++i) {
+    if (ht_entry(&m, &keys[i], &e)) {
+      if (ht_at(&m, &e)->value != values[i]) {
+        fprintf(stderr, "error: %lu != %lu\n", ht_at(&m, &e)->value, values[i]);
+        return EXIT_FAILURE;
+      }
+    } else {
+      fprintf(stderr, "error: key %lu not found\n", keys[i]);
+      return EXIT_FAILURE;
+    }
+  }
+
+  clock_gettime(CLOCK_MONOTONIC, &end);
+  ns = (end.tv_sec - start.tv_sec) * 1000000000UL + (end.tv_nsec - start.tv_nsec);
+  printf("lookup:\n  total: %lu ns\n  average: %lu ns\n", ns, ns / SIZE);
+
+  ht_deinit(&m);
+  free(keys);
+  free(values);
+
+  return EXIT_SUCCESS;
+}
---
+++ test/vec.c
@@ -0,0 +1,50 @@
+/*
+ * vec.c -- vector test
+ *
+ * SPDX-FileCopyrightText: 2026 Shota Minami
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "ds.h"
+
+#define SIZE 1000000
+
+int main(void)
+{
+  unsigned long i;
+  unsigned long *values;
+
+  vec(unsigned long) v;
+
+  values = malloc(sizeof(unsigned long) * SIZE);
+  for (i = 0; i < SIZE; ++i) {
+    values[i] = i;
+  }
+  for (i = 0; i + 1 < SIZE; ++i) {
+    unsigned long j = rand() % (SIZE - (i + 1)) + i;
+    unsigned long t = values[i];
+    values[i] = values[j];
+    values[j] = t;
+  }
+  for (i = 0; i < SIZE; ++i) {
+    vec_push(&v, &values[i]);
+  }
+
+  assert(v.count == SIZE);
+
+  for (i = 0; i < SIZE; ++i) {
+    if (*vec_at(&v, i) != values[i]) {
+      fprintf(stderr, "error: expected %lu, got %lu\n", values[i], *vec_at(&v, i));
+      return EXIT_FAILURE;
+    }
+  }
+
+  vec_init(&v);
+  free(values);
+
+  return EXIT_SUCCESS;
+}
