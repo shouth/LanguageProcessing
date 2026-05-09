026487dea6f57493b11a272373d06650e1084f47
Shota Minami <shouth373@gmail.com>
2026-05-09 19:12:56 +0900

    add type for compilation unit

---
+++ src/unit.c
@@ -0,0 +1,138 @@
+/*
+ * unit.c -- compilation unit
+ *
+ * SPDX-FileCopyrightText: 2026 Shota Minami
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "unit.h"
+#include "ds.h"
+#include "sym.h"
+#include "syn.h"
+
+size_t const UNIT_INDEX_NULL = -1ul;
+
+unit_item_ref_t const UNIT_ITEM_REF_NULL = { UNIT_INDEX_NULL };
+unit_scope_ref_t const UNIT_SCOPE_REF_NULL = { UNIT_INDEX_NULL };
+
+static hash_t node_hash(void const *item)
+{
+  struct syn_node *x = *(struct syn_node **) item;
+  hash_t h;
+  hash_init(&h);
+  hash_add(&h, &x, sizeof(struct syn_node *));
+  return h;
+}
+
+static int node_eq(void const *lhs, void const *rhs)
+{
+  struct syn_node *l = *(struct syn_node **) lhs;
+  struct syn_node *r = *(struct syn_node **) rhs;
+  return l == r;
+}
+
+void unit_init(struct unit *unit)
+{
+  vec_init(&unit->items);
+  vec_init(&unit->scopes);
+  ht_init(&unit->uses, node_hash, node_eq);
+  vec_init(&unit->unresolved);
+}
+
+void unit_deinit(struct unit *unit)
+{
+  size_t i;
+
+  for (i = 0; i < unit->items.count; ++i) {
+    struct unit_item *item = &unit->items.data[i];
+    vec_deinit(&item->children);
+    vec_deinit(&item->users);
+  }
+  vec_deinit(&unit->items);
+
+  for (i = 0; i < unit->scopes.count; ++i) {
+    struct unit_scope *scope = &unit->scopes.data[i];
+    vec_deinit(&scope->items);
+    vec_deinit(&scope->children);
+  }
+  vec_deinit(&unit->scopes);
+
+  ht_deinit(&unit->uses);
+  vec_deinit(&unit->unresolved);
+}
+
+unit_item_ref_t unit_add_item(struct unit *unit, struct sym const *name, enum unit_item_kind kind, struct syn_node const *ident, struct syn_node const *decl, unit_item_ref_t parent)
+{
+  unit_item_ref_t ref;
+
+  struct unit_item item;
+  item.name = name;
+  item.kind = kind;
+  item.ident = ident;
+  item.decl = decl;
+  item.parent = parent;
+  vec_init(&item.children);
+  vec_init(&item.users);
+  
+  ref.index = unit->items.count;
+  vec_push(&unit->items, &item);
+
+  if (parent.index != UNIT_INDEX_NULL) {
+    struct unit_item *x = vec_at(&unit->items, parent.index);
+    vec_push(&x->children, &ref);
+  }
+
+  return ref;
+}
+
+unit_scope_ref_t unit_add_scope(struct unit *unit, unit_scope_ref_t parent)
+{
+  unit_scope_ref_t ref;
+  
+  struct unit_scope scope;
+  vec_init(&scope.items);
+  scope.parent = parent;
+  vec_init(&scope.children);
+
+  ref.index = unit->scopes.count;
+  vec_push(&unit->scopes, &scope);
+
+  if (parent.index != UNIT_INDEX_NULL) {
+    struct unit_scope *x = vec_at(&unit->scopes, parent.index);
+    vec_push(&x->children, &ref);
+  }
+
+  return ref;
+}
+
+struct unit_item const *unit_get_item(struct unit *unit, unit_item_ref_t ref)
+{
+  return &unit->items.data[ref.index];
+}
+
+struct unit_scope const *unit_get_scope(struct unit *unit, unit_scope_ref_t ref)
+{
+  return &unit->scopes.data[ref.index];
+}
+
+void unit_add_unresolved(struct unit *unit, struct syn_node const *node)
+{
+  vec_push(&unit->unresolved, &node);
+}
+
+void unit_populate(struct unit *unit, unit_scope_ref_t scope, unit_item_ref_t item)
+{
+  struct unit_scope *x = vec_at(&unit->scopes, scope.index);
+  vec_push(&x->items, &item);
+}
+
+void unit_use(struct unit *unit, struct syn_node const *node, unit_item_ref_t item)
+{
+  struct ht_entry e;
+  struct unit_item *x = vec_at(&unit->items, item.index);
+  
+  ht_entry(&unit->uses, &node, &e);
+  ht_occupy(&unit->uses, &e, &node);
+  ht_at(&unit->uses, &e)->value = item;
+  vec_push(&x->users, &node);
+}
---
+++ src/unit.h
@@ -0,0 +1,72 @@
+/*
+ * unit.h -- compilation unit
+ *
+ * SPDX-FileCopyrightText: 2026 Shota Minami
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef UNIT_H
+#define UNIT_H
+
+#include <stddef.h>
+
+#include "ds.h"
+#include "sym.h"
+#include "syn.h"
+
+typedef struct { size_t index; } unit_item_ref_t;
+typedef struct { size_t index; } unit_scope_ref_t;
+
+extern unit_item_ref_t const UNIT_ITEM_REF_NULL;
+extern unit_scope_ref_t const UNIT_SCOPE_REF_NULL;
+
+enum unit_item_kind {
+  UNIT_ITEM_PROGRAM,
+  UNIT_ITEM_PROC,
+  UNIT_ITEM_VAR,
+  UNIT_ITEM_PARAM,
+  UNIT_ITEM_LOCAL
+};
+
+struct unit_item {
+  struct sym const *name;
+  enum unit_item_kind kind;
+  struct syn_node const *ident;
+  struct syn_node const *decl;
+  unit_item_ref_t parent;
+  vec(unit_item_ref_t) children;
+  vec(struct syn_node const *) users;
+};
+
+struct unit_scope {
+  vec(unit_item_ref_t) items;
+  unit_scope_ref_t parent;
+  vec(unit_scope_ref_t) children;
+};
+
+struct unit {
+  vec(struct unit_item) items;
+  vec(struct unit_scope) scopes;
+  hm(struct syn_node const *, unit_item_ref_t) uses;
+  vec(struct syn_node const *) unresolved;
+};
+
+void unit_init(struct unit *unit);
+
+void unit_deinit(struct unit *unit);
+
+unit_item_ref_t unit_add_item(struct unit *unit, struct sym const *name, enum unit_item_kind kind, struct syn_node const *ident, struct syn_node const *decl, unit_item_ref_t parent);
+
+unit_scope_ref_t unit_add_scope(struct unit *unit, unit_scope_ref_t parent);
+
+struct unit_item const *unit_get_item(struct unit *unit, unit_item_ref_t ref);
+
+struct unit_scope const *unit_get_scope(struct unit *unit, unit_scope_ref_t ref);
+
+void unit_add_unresolved(struct unit *unit, struct syn_node const *node);
+
+void unit_populate(struct unit *unit, unit_scope_ref_t scope, unit_item_ref_t item);
+
+void unit_use(struct unit *unit, struct syn_node const *node, unit_item_ref_t item);
+
+#endif /* UNIT_H */
